eab3e343bad12fba8189c71ef46e06b980bbd8ef
山雨欲来风满楼 <maqingshui@outlook.com>
2024-05-18 15:13:54 +0800

    Update morse.py
    优化代码，增加更多字符（符号）

--- morse.py
+++ morse.py
@@ -1,289 +1,87 @@
-import machine
-import utime
+"""
+# AutoMorse - Automatic Morse Code Transmitter for Raspberry Pi Pico
+# Copyright (c) 2024 MQSI/BG7ZDQ (maqingshui@outlook.com)
+# The Apache License 2.0 - see license file
+"""
+
 from machine import Pin
 from utime import sleep
 
-buzzer = Pin(16,Pin.OUT)
-led = Pin(17,Pin.OUT)
-
-#_.def
-def long():#长
-    print('-')
-    buzzer.value(1)
-    led.value(1)
-    sleep(0.15)
-    buzzer.value(0)
-    led.value(0)
-    sleep(0.07)
+class AutoMorse:
     
-def short():#短
-    print('.')
-    buzzer.value(1)
-    led.value(1)
-    sleep(0.075)
-    buzzer.value(0)
-    led.value(0)
-    sleep(0.07)
-    
-def lettersfeed():#分字
-    sleep(0.2)
-    
-def wordsfeed():#分词
-    sleep(0.1)
-    
-def error():#错误
-    print("\033[0;31;40merror\033[0m")
-    buzzer.value(1)
-    sleep(0.4)
-    buzzer.value(0)
-    sleep(1)
-    
-# Morse def
-def Morse(Phrase ):
-    for letter in Phrase:
-        print(letter)
-        if letter == 'A' :
-            short()#                .
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'B' :
-            long()#                 -
-            short()#                .
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'C' :
-            long()#                 -
-            short()#                .
-            long()#                 -
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'D' :
-            long()#                 -
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'E' :
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'F' :
-            short()#                .
-            short()#                .
-            long()#                 -
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'G' :
-            long()#                 -
-            long()#                 -
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'H' :
-            short()#                .
-            short()#                .
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'I' :
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'J' :
-            short()#                .
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'K' :
-            long()#                 -
-            short()#                .
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'L' :
-            short()#                .
-            long()#                 -
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'M' :
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'N' :
-            long()#                 -
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'O' :
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'P' :
-            short()#                .
-            long()#                 -
-            long()#                 -
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'Q' :
-            long()#                 -
-            long()#                 -
-            short()#                .
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'R' :
-            short()#                .
-            long()#                 -
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'S' :
-            short()#                .
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == 'T' :
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'U' :
-            short()#                .
-            short()#                .
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'V' :
-            short()#                .
-            short()#                .
-            short()#                .
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'W' :
-            short()#                .
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'X' :
-            long()#                 -
-            short()#                .
-            short()#                .
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'Y' :
-            long()#                 -
-            short()#                .
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == 'Z' :
-            long()#                 -
-            long()#                 -
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == '1' :
-            short()#                .
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == '2' :
-            short()#                .
-            short()#                .
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == '3' :
-            short()#                .
-            short()#                .
-            short()#                .
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == '4' :
-            short()#                .
-            short()#                .
-            short()#                .
-            short()#                .
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == '5' :
-            short()#                .
-            short()#                .
-            short()#                .
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == '6' :
-            long()#                 -
-            short()#                .
-            short()#                .
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == '7' :
-            long()#                 -
-            long()#                 -
-            short()#                .
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == '8' :
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            short()#                .
-            short()#                .
-            lettersfeed()
-            
-        elif letter == '9' :
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            short()#                .
-            lettersfeed()
-            
-        elif letter == '0' :
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            long()#                 -
-            lettersfeed()
-            
-        elif letter == ' ':
-            wordsfeed()
+    # Output port, external indication Led port, transmission speed
+    def __init__(self, buzzer_pin, led_pin, wpm):
+        self.buzzer = Pin(buzzer_pin, Pin.OUT)
+        self.led = Pin(led_pin, Pin.OUT)
         
-        else :
-            error()
+
+        # In standard conditions, the duration of a dot in Morse code is 1.2 times the words per minute (WPM) rate
+        # (1.2 is the standard time unit). The duration of a dash is 3 times the duration of a dot. The interval between
+        # letters is 3 times the duration of a dot, and the interval between words is 7 times the duration of a dot.
+        self.dot_length = 1.2 / wpm
+        self.dash_length = self.dot_length * 3
+        self.letter_space = self.dot_length * 3
+        self.word_space = self.dot_length * 7
+        
+        # dictionary mapping
+        self.morse_code = {
+            'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 
+            'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 
+            'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 
+            'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 
+            'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', 
+            '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', 
+            '9': '----.', '0': '-----', ',': '--..--', '.': '.-.-.-', '?': '..--..', 
+            '\'': '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', 
+            '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', 
+            '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', 
+            ' ': ' '
+        }
+
+    def long(self):
+        print('-')
+        self.buzzer.value(1)
+        self.led.value(1)
+        sleep(self.dash_length)
+        self.buzzer.value(0)
+        self.led.value(0)
+        sleep(self.dot_length)
+
+    def short(self):
+        print('.')
+        self.buzzer.value(1)
+        self.led.value(1)
+        sleep(self.dot_length)
+        self.buzzer.value(0)
+        self.led.value(0)
+        sleep(self.dot_length)
+
+    def lettersfeed(self):
+        sleep(self.letter_space - self.dot_length)
+
+    def wordsfeed(self):
+        sleep(self.word_space - self.dot_length)
+
+    def error(self):
+        print("error")
+        self.buzzer.value(1)
+        sleep(self.word_space)
+        self.buzzer.value(0)
+        sleep(self.letter_space)
+
+    def Morse(self, phrase):
+        for letter in phrase:
+            if letter in self.morse_code:
+                morse_representation = self.morse_code[letter]
+                print(letter, morse_representation)
+                for symbol in morse_representation:
+                    if symbol == '-':
+                        self.long()
+                    elif symbol == '.':
+                        self.short()
+                if letter != ' ':
+                    self.lettersfeed()
+                else:
+                    self.wordsfeed()
+            else:
+                self.error()
